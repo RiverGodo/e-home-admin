5050ee928114f5a69a662dbb80b7a53cb1ab562f
WangShaoqi <924384288@qq.com>
2018-10-30 22:08:00 +0800

    img

---
+++ controller/news.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('./auth');
+const newsModel = require('../model/news')
+
+
+router.post('/',auth, async(req, res, next) => {
+    try{
+        let{
+            title,
+            content,
+            contentText,
+            img,
+            author,
+            type,
+            look_num
+        } = req.body;
+
+        const news = await newsModel.create({
+            title,
+            content,
+            contentText,
+            img,
+            author,
+            type,
+            look_num
+        })
+
+        res.json({
+            code:200,
+            data:news,
+            msg:'新闻添加成功'
+        })
+    }catch(err){
+        next(err)
+    }
+})
+
+router.get('/',async (req, res, next) => {
+    try{
+        let {page = 1, page_size = 10} = req.query
+        page = parseInt(page)
+        page_size = parseInt(page_size)
+        
+        const dataList = await newsModel
+            .find()
+            .skip((page-1)*page_size)
+            .limit(page_size)
+            .sort({_id:-1})
+            .populate({
+                path:'admin_user',
+                select:"-password"
+            })
+            .populate({
+                path:'category'
+            })
+
+            res.json({
+                code:200,
+                data:dataList,
+                msg:'success'
+            })
+    }catch(err){
+        next(err)
+    }
+})
+
+router.get('/:id',async (req, res, next) => {
+    try{
+        const {id} = req.params
+        const data = await newsModel
+            .findById(id)
+            .populate({
+                path:'admin_user',
+                select:"-password"
+            })
+            .populate({
+                path:'category'
+            })
+
+            res.json({
+                code:200,
+                data,
+                msg:'success'
+            })
+    }catch(err){
+        next(err)
+    }
+})
+
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 router.use('/admin/adminUser',require('../controller/adminUser'))
+router.use('/admin/news', require('../controller/news'))
 
 module.exports = router;
